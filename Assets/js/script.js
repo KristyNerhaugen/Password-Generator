1accbe9a1bfdb34b83cbb355d8a4a8174f8e25b6
kristynerhaugen <kristynerhaugen@gmail.com>
2022-05-21 12:44:05 -0500

    fixed math floor issue code fully works

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,48 +16,51 @@ function writePassword() {
 function generatePassword() {
 
   // array for numbers
-  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   console.log(numbers);
 
   // array for lowercase letters
-  const lettersLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var lettersLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   console.log(lettersLower);
 
   // array for lowercase letters
-  const lettersUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lettersUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   console.log(lettersUpper);
 
   // array for special characters--can't get  ''' or '\' to work 
-  const specialCharacter = [' ', '!', '_', '"', '#', ']', '[', '$', '%', '&', ')', '(', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '`', '{', '{','|', '~'];
+  var specialCharacter = [' ', '!', '_', '"', '#', ']', '[', '$', '%', '&', ')', '(', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '`', '{', '{', '|', '~'];
   console.log(specialCharacter);
 
-  // Define blank variable for password criteria to attach to and return 
+  // Define blank variable for password criteria to attach to
   let passwordReturn = "";
+
+  // Created password pool to pull citeria into
+  var passwordPool = [];
 
   // Asking the user what they want in their password using window confirm
   if (window.confirm("Do you want numbers in your password?") === true) {
-    console.log(true);  
+    console.log(true);
     // Generate number(s) if true
-    passwordReturn += numbers;
+    passwordPool += numbers;
   }
 
- if (window.confirm("Do you want lowercase letters in your password?") === true) {
+  if (window.confirm("Do you want lowercase letters in your password?") === true) {
     console.log(true);
     // Generates lowercase letter(s) if true
-    passwordReturn += lettersLower;
+    passwordPool += lettersLower;
   }
 
   if (window.confirm("Do you want uppercase letters in your password?") === true) {
-      console.log(true);
-      // Generates uppercase letter(s) if true
-      passwordReturn += lettersUpper;
-    }
-   
+    console.log(true);
+    // Generates uppercase letter(s) if true
+    passwordPool += lettersUpper;
+  }
+
   if (window.confirm("Do you want special characters in your password?") === true) {
     console.log(true);
     // Generates special character(s) if true
-    passwordReturn += specialCharacter;
-    } 
+    passwordPool += specialCharacter;
+  }
 
   // Using window prompt to ask about password length preference 
   let length = 0;
@@ -70,20 +73,18 @@ function generatePassword() {
   console.log(length);
 
   // For loop 
-  for (var i = 0; i<= length; i++) {
-    passwordReturn = Mathfloor(Math.random() * 60);
+  for (var i = 0; i < length; i++) {
+    passwordReturn += passwordPool[Math.floor(Math.random()*passwordPool.length)];
   }
 
+  return passwordReturn;
   // return letters, numbers, characters by using return and Math, detailed in 4 lines below
- // return numbers[Math.floor(Math.random()*numbers.length)];
- // return lettersLower[Math.floor(Math.random()*lettersLower.length)];
- // return lettersUpper[Math.floor(Math.random()*lettersUpper.length)];
- // return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+  // return numbers[Math.floor(Math.random()*numbers.length)];
+  // return lettersLower[Math.floor(Math.random()*lettersLower.length)];
+  // return lettersUpper[Math.floor(Math.random()*lettersUpper.length)];
+  // return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Call writepassword function
-// writePassword();
